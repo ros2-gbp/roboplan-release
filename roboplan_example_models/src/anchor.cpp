4b60d07fffa885f902f8e8748a84da684adba14d
Sebastian Castro <sebas.a.castro@gmail.com>
2026-07-08 09:13:23 -0400

    Revert "Trimmed the branch to only the roboplan_examples sub directory"
    This reverts commit 119bc3e0be24f18b9a13c8e15af3327e78f295eb.

---
+++ roboplan_example_models/src/anchor.cpp
@@ -0,0 +1,8 @@
+namespace roboplan::example_models {
+namespace anchor {
+// This function exists solely as an anchor to locate the exported library on
+// disk at runtime. It must be compiled as a separate unit so that it is not
+// wrapped up in the python library.
+void example_models_location_anchor() {}
+}  // namespace anchor
+}  // namespace roboplan::example_models
