f5cf11ae21491fe4f1a168323871f3bff6ebc96f
Sebastian Castro <sebas.a.castro@gmail.com>
2026-07-08 09:10:53 -0400

    Trimmed the branch to only the roboplan sub directory

--- roboplan_simple_ik/bindings/python/roboplan/simple_ik/__init__.py
+++
@@ -1,6 +0,0 @@
-# The `roboplan.simple_ik` bindings, backed by `_simple_ik_ext`.
-# Import core first to guarantee its types are registered before use.
-import roboplan.core  # noqa: F401
-
-from ._simple_ik_ext import *  # noqa: E402,F401,F403
-from ._simple_ik_ext import __version__  # noqa: E402,F401
