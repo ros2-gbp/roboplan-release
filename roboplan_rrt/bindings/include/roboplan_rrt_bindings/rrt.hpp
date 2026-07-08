1db21e6a2c90a442bbe72d14042ac91d0c855aeb
Sebastian Castro <sebas.a.castro@gmail.com>
2026-07-08 09:16:06 -0400

    Rebase from 'upstream'

--- roboplan_rrt/bindings/include/roboplan_rrt_bindings/rrt.hpp
+++ roboplan_rrt/bindings/include/roboplan_rrt_bindings/rrt.hpp
@@ -6,6 +6,6 @@ namespace roboplan {
 
 /// @brief Initializes Python bindings for the RRT path planner.
 /// @param m The nanobind core module.
-void init_rrt(nanobind::module_& m);
+void initRrt(nanobind::module_& m);
 
 }  // namespace roboplan
