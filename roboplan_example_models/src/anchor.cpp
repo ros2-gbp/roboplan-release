b5116dcf35200ffb6f84840689ebc38f24d90644
Sebastian Castro <sebas.a.castro@gmail.com>
2026-07-31 21:39:48 -0400

    Trimmed the branch to only the roboplan_toppra sub directory

--- roboplan_example_models/src/anchor.cpp
+++
@@ -1,8 +0,0 @@
-namespace roboplan::example_models {
-namespace anchor {
-// This function exists solely as an anchor to locate the exported library on
-// disk at runtime. It must be compiled as a separate unit so that it is not
-// wrapped up in the python library.
-void exampleModelsLocationAnchor() {}
-}  // namespace anchor
-}  // namespace roboplan::example_models
