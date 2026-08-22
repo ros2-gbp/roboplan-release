40b4deac81080f9f196fd088fcd0f52a185f8b3d
Sebastian Castro <sebas.a.castro@gmail.com>
2026-08-21 21:14:08 -0400

    Trimmed the branch to only the roboplan_cartesian_planning sub directory

--- .docker/ubuntu/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Set the library paths for installed packages
-source /roboplan_env.sh
-echo "Set CMAKE_PREFIX_PATH and LD_LIBRARY_PATH for roboplan."
-
-# Execute the command passed into this entrypoint
-exec "$@"
