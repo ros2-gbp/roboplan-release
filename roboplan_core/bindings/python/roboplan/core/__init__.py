ddd1b5e8ecbc3998c4aaf7c65f19cdf80ee27d34
Sebastian Castro <sebas.a.castro@gmail.com>
2026-09-19 20:42:38 -0400

    Trimmed the branch to only the roboplan_example_models sub directory

--- roboplan_core/bindings/python/roboplan/core/__init__.py
+++
@@ -1,7 +0,0 @@
-# The `roboplan.core` bindings, backed by the compiled `_core_ext` module.
-from roboplan_common import add_dll_directories
-
-_dll_directories = add_dll_directories()
-
-from ._core_ext import *
-from ._core_ext import __version__  # noqa: F401
